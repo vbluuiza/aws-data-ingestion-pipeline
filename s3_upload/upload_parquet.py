e4ee8296e4e69627262251bdf42d95f48e721adb
vbluuiza <luizavieirabelo3008@gmail.com>
2025-06-18 20:04:08 -0300

    ♻️ refactor(config): remover uso de credenciais AWS do .env para aws configure
    - Remove o carregamento das credenciais AWS do arquivo .env
    - Atualiza os scripts para utilizar as credenciais configuradas via `aws configure` (AWS CLI)

--- s3_upload/upload_parquet.py
+++ s3_upload/upload_parquet.py
@@ -2,21 +2,22 @@
 import boto3
 from io import BytesIO
 from dotenv import load_dotenv
-from aws.session import get_s3_client
 import os
 
 load_dotenv()
 bucket =  os.getenv('s3_bucket')
 
+# Converte um DataFrame em parquet na memória e faz upload para S3.
 def upload_df_to_s3_parquet(df, bucket, s3_path):
-    # Converte um DataFrame em parquet na memória e faz upload para S3.
+    session = boto3.Session(profile_name='default') 
+    s3 = session.client('s3')
     
     # Converte para parquet em memória
     buffer = BytesIO()
     df.to_parquet(buffer, index=False)
     buffer.seek(0)
     
-    s3 =  get_s3_client()
+    
     s3.upload_fileobj(buffer, bucket, s3_path)
     print(f"[✔] Send to s3://{bucket}/{s3_path}")
     
